a22d89b144eb019fdf2e651d0c0850e17bbef755
Oguzhan Burak ISSI <oguzhan.issi@tuni.fi>
2021-12-05 14:55:00 +0200

    Rearranged defaultto() test cases for better report readability

--- test/defaultto.js
+++ test/defaultto.js
@@ -3,44 +3,27 @@ import assert from "assert"
 import chai from "chai"
 import {expect} from "chai"
 
-describe('defaultto()', function() {
-    it('positive integers', function() {
+// nominal
+describe('defaultto(), Nominal Input', function() {
+    it('Positive integers', function() {
         assert.equal(defaultTo(4,1), 4);
     });
-});
-
-describe('defaultto()', function() {
-    it('positive floats', function() {
+    it('Positive floats', function() {
         assert.equal(defaultTo(4.4,1.2), 4.4);
     });
-});
-
-describe('defaultto()', function() {
-    it('negative integers', function() {
+    it('Negative integers', function() {
         assert.equal(defaultTo(-4,-1), -4);
     });
-});
-
-describe('defaultto()', function() {
-    it('negative floats', function() {
+    it('Negative floats', function() {
         assert.equal(defaultTo(-4.4, -1.1), -4.4);
     });
-});
-
-describe('defaultto()', function() {
-    it('null value', function() {
+    it('Null value', function() {
         assert.equal(defaultTo(null, 1), 1);
     });
-});
-
-describe('defaultto()', function() {
     it('NaN value', function() {
         assert.equal(defaultTo(NaN, 1), 1);
     });
-});
-
-describe('defaultto()', function() {
-    it('undefined value', function() {
+    it('Undefined value', function() {
         assert.equal(defaultTo(undefined, 1), 1);
     });
 });

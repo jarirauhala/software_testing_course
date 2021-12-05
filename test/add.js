57e601b140e0d09d4a2c39f30740ccb23fa2a436
Oguzhan Burak ISSI <oguzhan.issi@tuni.fi>
2021-12-05 14:43:43 +0200

    Rearranged add() test cases for better report readability

--- test/add.js
+++ test/add.js
@@ -4,122 +4,62 @@ import chai from "chai"
 import {expect} from "chai"
 
 
-// integers
+// nominal
 describe('add()', function() {
-    it('positive integers', function() {
+    it('Positive integers', function() {
         assert.equal(add(2,3), 5);
     });
-});
-
-describe('add()', function() {
-    it('negative integers', function() {
+    it('Negative integers', function() {
         assert.equal(add(2,3), 5);
     });
-});
-
-describe('add()', function() {
-    it('negative and positive integer', function() {
+    it('Negative and positive integer', function() {
         assert.equal(add(-2,3), 1);
     });
-});
-
-describe('add()', function() {
-    it('positive and negative integer', function() {
+    it('Positive and negative integer', function() {
         assert.equal(add(2,-3), -1);
     });
-});
-
-describe('add()', function() {
-    it('integer and zero', function() {
+    it('Integer and zero', function() {
         assert.equal(add(3,0), 3);
     });
-});
-
-describe('add()', function() {
-    it('zero and integer', function() {
+    it('Zero and integer', function() {
         assert.equal(add(0,3), 3);
     });
-});
-
-describe('add()', function() {
-    it('char with integer', function() {
-        expect(function(){add('a',3)}).to.throw(TypeError)
-    });
-});
-
-describe('add()', function() {
-    it('integer with char', function() {
-        expect(function(){add(3,'a')}).to.throw(TypeError)
-    });
-});
-
-// floats
-describe('add()', function() {
-    it('positive floats', function() {
+    it('Positive floats', function() {
         assert.equal(add(2.0,3.0), 5.0);
     });
-});
-
-describe('add()', function() {
-    it('negative floats', function() {
+    it('Negative floats', function() {
         assert.equal(add(2.0,3.0), 5.0);
     });
-});
-
-describe('add()', function() {
-    it('negative and positive float', function() {
+    it('Negative and positive float', function() {
         assert.equal(add(-2.0,3.0), 1.0);
     });
-});
-
-describe('add()', function() {
-    it('positive and negative float', function() {
+    it('Positive and negative float', function() {
         assert.equal(add(2.0,-3.0), -1.0);
     });
-});
-
-describe('add()', function() {
-    it('float and zero', function() {
+    it('Float and zero', function() {
         assert.equal(add(3.0,0), 3.0);
     });
-});
-
-describe('add()', function() {
-    it('zero and float', function() {
+    it('Zero and float', function() {
         assert.equal(add(0,3.0), 3.0);
     });
 });
 
+
+// erroneous
 describe('add()', function() {
-    it('char with float', function() {
+    it('Should throw when one argument is a char', function() {
         expect(function(){add('a',3.0)}).to.throw(TypeError)
-    });
-});
-
-describe('add()', function() {
-    it('float with char', function() {
         expect(function(){add(3.0,'a')}).to.throw(TypeError)
-    });
-});
-
-
-// special cases
-describe('add()', function() {
-    it('char with char', function() {
         expect(function(){add('b','a')}).to.throw(TypeError)
+        expect(function(){add(3,'a')}).to.throw(TypeError)
+        expect(function(){add('a',3)}).to.throw(TypeError)
     });
-});
-
-describe('add()', function() {
-    it('large numbers', function() {
+    it('Should throw with large numbers', function() {
         expect(function(){
             add(30000000000000000000000000000000000000000000000000000,2000000000000000000000000000000000000000)
         }).to.throw(RangeError)
     });
-});
-
-describe('add()', function() {
-    it('small numbers', function() {
+    it('Should throw with small numbers', function() {
         expect(function(){
             add(0.000000000000000000000000000000000000000000000000000000003,0.000000000000000000000000000000000000000000001)
         }).to.throw(RangeError)

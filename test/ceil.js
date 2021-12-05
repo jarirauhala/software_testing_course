6fffb16f2f025dac217cf60695fd875d7ae96d25
Oguzhan Burak ISSI <oguzhan.issi@tuni.fi>
2021-12-05 14:47:38 +0200

    Rearranged ceil() test cases for better report readability

--- test/ceil.js
+++ test/ceil.js
@@ -3,60 +3,38 @@ import assert from "assert"
 import chai from "chai"
 import {expect} from "chai"
 
-// positive numbers
-describe('ceil()', function() {
-    it('positive number, no precision', function() {
+// nominal
+describe('ceil(), Nominal Input', function() {
+    it('Positive number, no precision', function() {
         assert.equal(ceil(3.6), 4);
     });
-});
-
-describe('ceil()', function() {
-    it('positive number, positive precision', function() {
+    it('Positive number, positive precision', function() {
         assert.equal(ceil(3.001, 2), 3.01);
     });
-});
-
-describe('ceil()', function() {
-    it('positive number, negative precision', function() {
+    it('Positive number, negative precision', function() {
         assert.equal(ceil(5030, -2), 5100);
     });
-});
-
-// negative number
-describe('ceil()', function() {
-    it('negative number, no precision', function() {
+    it('Negative number, no precision', function() {
         assert.equal(ceil(-3.6), -3);
     });
-});
-
-describe('ceil()', function() {
-    it('negative number, positive precision', function() {
+    it('Negative number, positive precision', function() {
         assert.equal(ceil(-3.001, 2), -3);
     });
-});
-
-describe('ceil()', function() {
-    it('negative number, negative precision', function() {
+    it('Negative number, negative precision', function() {
         assert.equal(ceil(-5030, -2), -5000);
     });
 });
 
 
-// incorrect inputs
-describe('ceil()', function() {
-    it('char precision', function() {
+// erroneous
+describe('ceil(), Erroneous Input', function() {
+    it('Should throw when one argument is a char', function() {
         expect(function(){ceil(3, 'a')}).to.throw(TypeError)
     });
-});
-
-describe('ceil()', function() {
-    it('float precision', function() {
+    it('Should throw when float precision is critical', function() {
         expect(function(){ceil(3, 4.45345)}).to.throw(TypeError)
     });
-});
-
-describe('ceil()', function() {
-    it('undefined number', function() {
+    it('Should throw when one argument is undefined', function() {
         console.log(ceil(undefined))
         expect(function(){ceil(undefined)}).to.throw(TypeError)
     });

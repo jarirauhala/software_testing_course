462d3acdf104bb57a765c6e857c833bacc1a1c44
Oguzhan Burak ISSI <oguzhan.issi@tuni.fi>
2021-12-05 14:56:15 +0200

    Rearranged keys() test cases for better report readability

--- test/keys.js
+++ test/keys.js
@@ -3,20 +3,15 @@ import assert from "assert"
 import chai from "chai"
 import {expect} from "chai"
 
-describe('keys()', function() {
-    it('undefined value', function() {
+// nominal
+describe('keys(), Nominal Input', function() {
+    it('Undefined value', function() {
         assert.deepEqual(keys('abc'), [ '0', '1', '2' ]);
     });
-});
-
-describe('keys()', function() {
     it('NaN', function() {
         assert.deepEqual(keys(NaN), []);
     });
-});
-
-describe('keys()', function() {
-    it('function', function() {
+    it('Function', function() {
         function Foo() {
           this.a = 1
           this.b = 2
